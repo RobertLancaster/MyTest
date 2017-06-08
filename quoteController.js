7a739c448e114f4abb63c4206153008a9e7a407e
RobertLancaster <robert@baconlabs.io>
2017-06-08 10:57:12 -0700

    new commit

---
+++ quoteController.js
@@ -0,0 +1,11 @@
+/*
+function storeQuote(id, source, person, quote){
+  window.alert("Success!");
+  return null;
+}
+*/
+
+function storeQuote(source, person, quote){
+  window.alert(source, person, quote);
+  return null;
+}
